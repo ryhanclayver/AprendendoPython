3c439534fc1975e69d6c3737e05d37bec2350e07
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2023-11-11 21:24:51 -0300

    Add files via upload

---
+++ exBoasVindas.py
@@ -0,0 +1,2 @@
+nome=input('Digite o seu nome: ')
+print('Boas-vindas,', nome, '! Prazer em te conhecer!')
---
+++ teste01.py
@@ -0,0 +1,4 @@
+nome=input('qual e o seu nome?')
+idade=input('qual e a sua idade?')
+altura=input('qual é a sua altura?')
+print(nome, idade, altura)
