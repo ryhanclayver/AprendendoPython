ea8913ad185cbd5efc5c9e1053c0b322b5a72c1a
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2023-11-13 14:40:57 -0300

    Add files via upload

---
+++ exRaiz.py
@@ -0,0 +1,10 @@
+# Crie um algoritmo que leia um número e
+# mostre seu dobro, triplo e raiz quadrada
+
+n1 = int(input('Digite um número: '))
+dobro = n1 * n1
+triplo = n1 * n1 * n1
+raiz = n1 ** (1/2)
+
+print('Dobro: {}\nTriplo: {}\nRaiz: {}'.format(dobro, triplo, raiz))
+
