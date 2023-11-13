5df21618d4ef4c4d4b3da2ab44d99b3986c2157a
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2023-11-13 13:45:40 -0300

    Update exSoma.py

--- exSoma.py
+++ exSoma.py
@@ -1,5 +1,5 @@
 num1 = int(input('Digite o primeiro número: '))
-num2 = int (input('Digite o segundo número: '))
+num2 = int(input('Digite o segundo número: '))
 
 print('{} + {} = {}'.format(num1, num2, num1+num2))
 
