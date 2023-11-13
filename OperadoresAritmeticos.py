6d5114d4251f60c8192c11ab42bc5ced1869db15
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2023-11-13 14:29:20 -0300

    Add files via upload

---
+++ OperadoresAritmeticos.py
@@ -0,0 +1,12 @@
+# Ordem de Precedência
+
+# () parênteses
+# ** potência
+# * / % // multiplicação, divisão, resto da divisão, divisão inteira
+# + - soma, subtração
+
+print(81 ** (1 / 2), end=' ')  # raiz quadrada - end='' não deixa quebrar linha entre prints
+print(81 ** (1 / 3), end='>>>>>>>>>>>>>>>>')  # raiz cúbica
+n1 = 2
+n2 = 3
+print('\nDivisão: {:.2f}'.format(n1/n2))
---
+++ exSucessorAntecessor.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+suc = n1+1
+ant = n1-1
+print('O antecessor de {} é {}\nO sucessor de {} é {}'.format(n1, ant, n1, suc))
+
