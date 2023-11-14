641452e2fccb9f6b68e53d4df3a39f7f3e29ca1d
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2023-11-14 08:07:16 -0300

    Update exTabuada.py

--- exTabuada.py
+++ exTabuada.py
@@ -1,22 +1,17 @@
 n1 = int(input('Digite um número: '))
-n2 = n1 * 2
-n3 = n1 * 3
-n4 = n1 * 4
-n5 = n1 * 5
-n6 = n1 * 6
-n7 = n1 * 7
-n8 = n1 * 8
-n9 = n1 * 9
+
 print('TABUADA DO {}'.format(n1))
+print('='*12)
 print('{} X 0 = 0'.format(n1))
 print('{} X 1 = {}'.format(n1, n1))
-print('{} X 2 = {}'.format(n1, n2))
-print('{} X 3 = {}'.format(n1, n3))
-print('{} X 4 = {}'.format(n1, n4))
-print('{} X 5 = {}'.format(n1, n5))
-print('{} X 6 = {}'.format(n1, n6))
-print('{} X 7 = {}'.format(n1, n7))
-print('{} X 8 = {}'.format(n1, n8))
-print('{} X 9 = {}'.format(n1, n9))
+print('{} X 2 = {}'.format(n1, (n1*2)))
+print('{} X 3 = {}'.format(n1, (n1*3)))
+print('{} X 4 = {}'.format(n1, (n1*4)))
+print('{} X 5 = {}'.format(n1, (n1*5)))
+print('{} X 6 = {}'.format(n1, (n1*6)))
+print('{} X 7 = {}'.format(n1, (n1*7)))
+print('{} X 8 = {}'.format(n1, (n1*8)))
+print('{} X 9 = {}'.format(n1, (n1*9)))
+print('='*12)
 
 
