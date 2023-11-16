9e82709e515f939b7f2d0571cc0a67469626a107
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2023-11-16 14:29:21 -0300

    Add files via upload

---
+++ Import.py
@@ -0,0 +1,21 @@
+from math import sqrt
+import random
+import emoji
+
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print(f'A raiz de {num} é: {raiz:.2f}')
+
+#  math.floor arredonda para baixo
+#  math.ceil para cima
+
+num1 = random.random()
+num2 = random.randint(1, 10)
+print(num1)
+print(num2)
+
+print('Olá 🤡')
+
+
+
+
