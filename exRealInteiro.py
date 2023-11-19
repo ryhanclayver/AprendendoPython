1e5b6308f6d89d30b0751902a16a14b6153e3800
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2023-11-19 18:21:43 -0300

    Update exRealInteiro.py

--- exRealInteiro.py
+++ exRealInteiro.py
@@ -1,3 +1,10 @@
+from math import trunc
+
+num1 = float(input('Digite um número real: '))
+print(f'O número {num1} tem a parte inteira {trunc(num1)}')
+
+'''Duas formas de fazer esse exercício '''
+
 num = float(input('Digite um número real: '))
 numInt = int(num)
 print(f'O número {num} tem a parte inteira {numInt}')
