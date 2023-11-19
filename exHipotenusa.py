535e326598c6a39050ef75c29c3756df06f9b203
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2023-11-19 18:26:48 -0300

    Update exHipotenusa.py

--- exHipotenusa.py
+++ exHipotenusa.py
@@ -1,5 +1,5 @@
-import math
+from math import hypot
 
 cOposto = float(input('Digite o cateto oposto: '))
 cAdjacente = float(input('Digite o cateto adjacente: '))
-print(f'Hipotenusa: {math.hypot(cOposto, cAdjacente):.2f}')
+print(f'Hipotenusa: {hypot(cOposto, cAdjacente):.2f}')
