1f0eae9b7cd08651756d8086d77c9dcac03cf1c4
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2024-01-04 18:46:18 -0300

    Add files via upload

---
+++ exSilva.py
@@ -0,0 +1,6 @@
+nome = input('Digite o seu nome:')
+
+if 'silva' in nome or 'Silva' in nome or 'SILVA' in nome:
+    print('Você tem "Silva" no nome')
+else:
+    print('Você não tem "Silva" no nome')
