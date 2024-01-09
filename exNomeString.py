b6292a536b1f87163f2565aa14248b34511bddea
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2024-01-09 19:00:58 -0300

    Update exNomeString.py

--- exNomeString.py
+++ exNomeString.py
@@ -1,9 +1,9 @@
-nome = input('Digite o nome: ')
-print(nome.upper())
-print(nome.lower())
-print(len(nome) - nome.count(' '))
+nome = str(input('Digite o nome: ')).strip()
+print(f'seu nome em maiuculas é {nome.upper()}')
+print(f'seu nome em minusculas é {nome.lower()}')
+print(f'seu nome ao todo tem {len(nome) - nome.count(' ')} letras')
 primeiroNome = nome.split()[0]
-print(len(primeiroNome))
+print(f'tamanho do primeiro nome é {len(primeiroNome)}')
 
 
 
