312aee639818c930fdbc6e9f0a8dc9a25f158f49
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2024-01-09 19:21:39 -0300

    Update exSilva.py

--- exSilva.py
+++ exSilva.py
@@ -1,6 +1,2 @@
-nome = input('Digite o seu nome:')
-
-if 'silva' in nome or 'Silva' in nome or 'SILVA' in nome:
-    print('Você tem "Silva" no nome')
-else:
-    print('Você não tem "Silva" no nome')
+nome = str(input('Digite o seu nome:')).strip()
+print(f'Existe "SILVA" no seu nome? {'silva' in nome.lower()}')
