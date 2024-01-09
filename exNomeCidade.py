ec19da6416b59fb9aff706494ef657d69fc0c839
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2024-01-09 19:14:43 -0300

    Update exNomeCidade.py

--- exNomeCidade.py
+++ exNomeCidade.py
@@ -1,5 +1,2 @@
-nome = input('Digite o nome da sua cidade: ')
-if 'santo' not in nome.split()[0] and 'Santo' not in nome.split()[0] and 'SANTO' not in nome.split()[0]:
-    print('O nome da sua cidade não começa com Santo')
-else:
-    print('O nome da sua cidade começa com Santo')
+nome = str(input('Digite o nome da sua cidade: ')).strip()
+print(nome[0:5].upper() == 'SANTO')
