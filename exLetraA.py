81136caf6476a9a7c3f4c133cba5cd0cc6ff93e1
Ryhan Clayver <106684262+ryhanclayver@users.noreply.github.com>
2024-01-09 19:24:48 -0300

    Update exLetraA.py

--- exLetraA.py
+++ exLetraA.py
@@ -1,5 +1,5 @@
-frase = input('Digite uma frase: ')
-a = frase.count('a')+frase.count('A')
-print(f'Apareceram {a} letras "A"')
+frase = str(input('Digite uma frase: ')).strip()
+a = frase.lower()
+print(f'Apareceram {a.count('a')} letras "A"')
 print(f'Ela aparece pela primeira vez na posição {frase.find('a')}')
 print(f'Ela aparece pela última vez na posição {frase.rfind('a')}')
